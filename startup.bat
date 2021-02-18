b4fb6e9be775c1bd9d9151e7a1d5c12efa53e8e0
Brandon Steed <brdsteed@gmail.com>
2021-02-17 20:54:30 -0500

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,4 +1,8 @@
 Rem This starts up the database	
-
 ECHO OFF
-
+cd php
+ECHO ------------------------------
+ECHO Starting server...
+ECHO Press Ctrl+C to close server. 
+ECHO ------------------------------
+.\php -S localhost:8080
