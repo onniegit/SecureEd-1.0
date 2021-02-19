61f874d765ee69b5aef81f64d4b7f8eb9f824b56
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-02-18 23:22:13 -0500

    Update DBcontents.php

--- test/DBcontents.php
+++ test/DBcontents.php
@@ -1,4 +1,27 @@
 <?php
+include('../config/ConfigV2.php');
+
+$sql =<<<EOF
+Select * From User;
+EOF;
+Global $db;
+$ret = $db->query($sql);
+while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+{
+    echo "Email = " . $row['Email'] . "\n";
+    echo "AccountType = " . $row['AccountType'] . "\n";
+    echo "Password = " . $row['Password'] . "\n";
+    echo "Name = " . $row['Name'] . "\n";
+    echo "DOB = " . $row['DOB'] . "\n";
+    echo "Year = " . $row['Year'] . "\n";
+    echo "Rank = " . $row['Rank'] . "\n";
+    echo "Security Question = " . $row['SQuestion'] . "\n";
+    echo "Security Answer = " . $row['SAnswer'] . "\n";
+}
 
 
+
+//Select * From Course;
+//Select * From CourseEnroll;
+//Select * From Grade;
 ?>
