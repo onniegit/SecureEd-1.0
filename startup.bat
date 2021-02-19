4c874cbd5c4abf9b8f6d081c32d1c3bd9d8f427b
Brandon Steed <brdsteed@gmail.com>
2021-02-18 22:49:14 -0500

    Update startup.bat
    Removed extraneous text prompts - closing terminal ends webserver session without prompting.

--- startup.bat
+++ startup.bat
@@ -1,7 +1,5 @@
-Rem This starts up the database	
-ECHO OFF
-ECHO ------------------------------
-ECHO Starting server...
-ECHO Press Ctrl+C to close server. 
-ECHO ------------------------------
+Rem "This starts up the database"
+
 .\php -S localhost:8000
+
+Rem "Note that when you close the window, the server closes automatically"
