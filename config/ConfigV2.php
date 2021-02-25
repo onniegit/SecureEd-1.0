c973ed9cb3d542ece0b7c696a4e23d5ac8700061
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-02-25 17:59:38 -0500

    Update ConfigV2.php

--- config/ConfigV2.php
+++ config/ConfigV2.php
@@ -8,7 +8,7 @@
    	} 
 	else 
 	{
-      //	echo "Opened database successfully\n";
+      	echo "Opened database successfully\n";
    	}
 
    $sql =<<<EOF
@@ -17,57 +17,86 @@
 	Email		TEXT 	PRIMARY KEY     NOT NULL	UNIQUE,
 	AccountType    	TEXT    NOT NULL,
 	Password    	TEXT    NOT NULL,      		
-	FName           	TEXT    NOT NULL,
-	LName           	TEXT    NOT NULL,
-	DOB				TEXT,
+	FName          	TEXT    NOT NULL,
+	LName           TEXT    NOT NULL,
+	DOB		TEXT	NOT NULL,
       	Year		INT,
 	Rank		TEXT,
 	SQuestion	TEXT	NOT NULL,
 	SAnswer		TEXT	NOT NULL
 	);
-      
-	CREATE TABLE Course
-      	(
-	CourseID	INT 	PRIMARY KEY     NOT NULL	UNIQUE,
+
+CREATE TABLE Course
+    (
+	CourseID	    INT 	PRIMARY KEY     NOT NULL	UNIQUE,
 	CourseName      TEXT    NOT NULL,
 	Semester    	TEXT    NOT NULL,
 	StartTime	TIME	NOT NULL,
 	EndTime		TIME	NOT NULL,
-      	Location	TEXT	NOT NULL,
+    	Location	TEXT	NOT NULL,
+	StartTime	TEXT	NOT NULL,
+	EndTime		TEXT	NOT NULL,
+    	Location	TEXT	NOT NULL,
 	Professor    	TEXT,      		
-	FOREIGN KEY (Professor) REFERENCES User (NAME) ON
-    DELETE SET NULL ON UPDATE CASCADE
+	FOREIGN KEY (Professor) REFERENCES User (Email) ON
+    		DELETE SET NULL ON UPDATE CASCADE
+	);
+    
+CREATE TABLE CourseEnroll
+    	(
+	CourseID	INT 	PRIMARY KEY     NOT NULL	UNIQUE,
+	Student		TEXT    NOT NULL,
+	MaxSize    	INT     NOT NULL,		
+	FOREIGN KEY (Student) REFERENCES User (Email) ON
+    		DELETE SET NULL ON UPDATE CASCADE
 	);
 	
-	CREATE TABLE Grade
+CREATE TABLE Grade
       	(
 	CourseID	INT     NOT NULL	UNIQUE,
-	StudentEmail     TEXT    NOT NULL,
-      	Grade		Text	NOT NULL,
+	StudentEmail	TEXT    NOT NULL,
+    	LetterGrade	Text	NOT NULL,
 	PRIMARY KEY(CourseID,StudentEmail),  		
 	FOREIGN KEY (StudentEmail) REFERENCES User (Email) ON
-    DELETE SET NULL ON UPDATE CASCADE
+    		DELETE SET NULL ON UPDATE CASCADE
 	);
 
 
-	INSERT INTO USER (Email,AccountType,Password,FName, LName, DOB,Year,Rank,SQuestion,SAnswer)
-      	VALUES ('Admin@email.com','Admin','Password1','John','Doe','2001-05-10 00:00:00',NULL,NULL,'Favorite Relative?','Bobsmyuncle');
+/*--------------User Values-----------------*/
+	INSERT INTO User (Email, AccountType, Password, Name, DOB, Year, Rank, SQuestion, SAnswer)
+      	VALUES ('Admin@email.com', 'Admin', 'Password1', 'John', '2001-05-10', NULL, NULL, 'Favorite Relative?', 'Bobsmyuncle');
 
-	INSERT INTO USER (Email, AccountType, Password, FName, LName, DOB, Year, Rank, SQuestion, SAnswer)
-      	VALUES ('DrProfessor@email.com', 'Faculty', 'Password2', 'Mister','DrProfessor', '1932-09-01', NULL, 'Professor', 'Favorite Relative?', 'Notmyuncle');
+
 	
-	INSERT INTO USER (Email, AccountType, Password, FName, LName, DOB, Year, Rank, SQuestion, SAnswer)
-      	VALUES ('Student@email.com', 'Student', 'Password3', 'Pepe','Frog', '2002-06-12', '3', NULL, 'Favorite Relative?', 'JoeyBatey');
+	INSERT INTO User (Email, AccountType, Password, Name, DOB, Year, Rank, SQuestion, SAnswer)
+      	VALUES ('Student@email.com', 'Student', 'Password3', 'Pepe', '2002-06-12', '3', NULL, 'Favorite Relative?', 'JoeyBatey');
+
+
+/*--------------Course Values-----------------*/
+	INSERT INTO Course (CourseID, CourseName, Semester, StartTime, EndTime, Location, Professor)
+      	VALUES ('123', 'Intro to CyberSecurity', 'Fall 2030', '08:30:00', '09:45:00', 'Building A', 'DrProfessor@email.com');
+
+	INSERT INTO Course (CourseID, CourseName, Semester, StartTime, EndTime, Location, Professor)
+      	VALUES ('456', 'Intermediate CyberSecurity', 'Spring 2030', '13:30:00', '14:45:00', 'Building A', 'DrProfessor@email.com');
+
+
+/*--------------CourseEnroll Values-----------------*/
+	INSERT INTO CourseEnroll (CourseID, Student, MaxSize)
+      	VALUES ('123', 'Student@email.com', '30');
+
+	INSERT INTO CourseEnroll (CourseID, Student, MaxSize)
+      	VALUES ('456', 'Student@email.com', '30');
+
+
+/*--------------Grade Values-----------------*/
+	INSERT INTO Grade (CourseID, StudentEmail, LetterGrade)
+      	VALUES ('123', 'Student@email.com', 'D');
+
+	INSERT INTO Grade (CourseID, StudentEmail, LetterGrade)
+      	VALUES ('456', 'Student@email.com', 'F');
+
 EOF;
 
-<<<<<<< Updated upstream
-   $ret = $db->exec($sql);
-   if(!$ret){
-      echo $db->lastErrorMsg();
-   } else {
-     // echo "Table created successfully\n";
-   }
-=======
     $ret = $db->exec($sql);
     echo "Config attempt...\n";
     if (!$ret) 
@@ -78,7 +107,4 @@ EOF;
 	{
         echo "Table created successfully\n";
     	}
-    ;
-
->>>>>>> Stashed changes
 ?>
