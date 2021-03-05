3902e4858dbf5a2f04534593cf36a491aeb9d7d8
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-03-04 19:11:45 -0500

    tested and added comments to forgot passwordbackendlogic

--- src/ForgotPasswordBackendLogic.php
+++ src/ForgotPasswordBackendLogic.php
@@ -2,7 +2,7 @@
 //ensuring database connection
     include_once '../config/ConfigV2.php';
 
-//Variables and Email gained from user entry
+//Variables and Email gained from user entry------------------
 
 $email = strtolower($_POST['Email']);
 $SecQuestion = "";
@@ -11,37 +11,48 @@ $mySAnswer = "";
 $NewPassword = "";
 $NewPasswordConfirm = "";
 
+//checks if given email exists-------------
 $query = "SELECT COUNT(*) as count FROM User WHERE Email ='$email'";
 $count = $db->querySingle($query);
 
-if($count==1)
-	{
-	
+if($count !=1)
+{echo 'Invalid Email';}
+
+else	
+{	
+     //finds security questions and answers of given email---------------
 	$query = "SELECT SQuestion FROM User WHERE Email ='$email'";
     	$SecQuestion = $db->query($query);
 	
 	$query = "SELECT SAnswer FROM User WHERE Email ='$email'";
     	$SecAnswer = $db->query($query);
 
+
+     //reads answer from user---------------------
 	$mySAnswer = $_POST['SAnswer'];
 
-	
+     //checks if anwser is correct---------------
 	if($mySAnswer != $SecAnswer)
 		{echo 'Invalid Answer';}
+	
 	else 
 		{
+		     //reads new password and confirm password---------------
 			$NewPassword = $_POST['newpassword'];	
 			$NewPasswordConfirm = $_POST['newpasswordconfirm'];
-
-			if($NewPassword == $NewPasswordConfirm)
-				{
+                     
+		      //checks if new password matches with confirm password------------
+			if($NewPassword != $NewPasswordConfirm)
+				{echo 'Passwords do not match';}
+			
+			else 
+				{				      
+				      //updates password-----------------
 					$query = "UPDATE User SET Password = $NewPassword WHERE Email = '$email'";
 					$db->exec($query);
 				}
-			else {echo 'Passwords do not match';}
 
 		}
-	}
-	
-else {echo 'Invalid Email';}
+}
 
+?>
