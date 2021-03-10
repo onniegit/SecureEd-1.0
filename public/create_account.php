a3242dc31567d0ef61f43109f79ec7c71944ec32
Brandon Steed <brdsteed@gmail.com>
2021-03-09 19:11:02 -0500

    Work on create account
    Not functional yet.

---
+++ public/create_account.php
@@ -0,0 +1,349 @@
+<?php
+/*Check if DB was initialized and grab link*/
+include_once '../config/ConfigV2.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="secure_app.css">
+    <meta charset="utf-8" />
+    <title>Secure App - Create Account</title>
+
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <table class="header_table">
+
+            <tbody>
+            <tr>
+
+                <td class="lock"><img src="Header_Lock_Image.svg" style="width:9vh;" alt=""></td>
+
+                <td class="title"><b>Secure App</b></td>
+
+                <td class="header_table_cell"></td>
+
+            </tr>
+            </tbody>
+        </table>
+    </header>
+
+    <main>
+        <!--Navigation Buttons-->
+        <nav>
+            <button class="button_large" type="button" onclick=" location.href = 'dashboard.php'">Dashboard</button>
+            <button class="button_large" type="button" onclick=" location.href = 'index.php'">Log Out</button>
+        </nav>
+
+        <br>
+
+        <!--Heading-->
+        <h1>Create Account</h1>
+        <div class=horizontal_line>
+            <hr>
+        </div>
+
+        <div style="text-align:center">
+            <div class = "edit_acc_pane">
+                <form action="../src/EditAccountUpdateLogic.php" method="POST" id="createform">
+                    <label class="edit_acc_label">Account type:</label>
+                    <select name="acctype" id="acctype" onchange="swapselection()">
+                            <optgroup label="Choose one">
+                                <option selected="selected" value="Faculty" >Faculty</option>
+                                <option value="Student">Student</option>
+                            </optgroup>
+                    </select>
+                <div class=horizontal_line>
+                    <hr>
+                </div>
+                <!--Input boxes-->
+                    <table>
+                        <tbody>
+                        <tr>
+                            <!--First Name-->
+                            <td>
+                                <label class = "edit_acc_label"> First Name: </label>
+                            </td>
+                            <td>
+                                <input type="text" id="fname" name="fname" value="<?php if(!$error){echo "$userinfo[3]";} ?>">
+                            </td>
+
+                            <!--Last Name-->
+                            <td>
+                                <label class = "edit_acc_label"> Last Name: </label>
+                            </td>
+                            <td>
+                                <input type="text" id="lname" name="lname" value="<?php if(!$error){echo "$userinfo[4]";} ?>">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Date of Birth-->
+                            <td>
+                                <label class = "edit_acc_label"> Date of Birth: </label>
+                            </td>
+                            <td>
+                                <input type="date" id="dob" name="dob" value="<?php if(!$error){echo $userinfo[5];} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Faculty Rank/Student Year-->
+                            <td>
+                                <label class = "edit_acc_label" id="positionlabel"> <?php if($userinfo[1]==="Student"){echo "Year:";} else {echo "Rank:";}?> </label>
+                            </td>
+                            <td>
+                                <select name="studentyear" id="studentyear" style = "display:none;">
+                                    <optgroup label="Student">
+                                        <option selected="selected" value="1" <?php if($userinfo[6] == 1){echo "selected";} ?>>Freshman</option>
+                                        <option value="2" <?php if($userinfo[6] == 2){echo "selected";} ?>>Sophomore</option>
+                                        <option value="3" <?php if($userinfo[6] == 3){echo "selected";} ?>>Junior</option>
+                                        <option value="4" <?php if($userinfo[6] == 4){echo "selected";} ?>>Senior</option>
+                                    </optgroup>
+                                </select>
+                                <select name="facultyrank" id="facultyrank" style = "display:block;">
+                                    <optgroup label="Faculty">
+                                        <option selected="selected" value="Instructor" <?php if($userinfo[7] === "Instructor"){echo "selected";} ?>>Instructor</option>
+                                        <option value="Adjunct" <?php if($userinfo[7] === "Adjunct"){echo "selected";} ?>>Adjunct Professor</option>
+                                        <option value="Assistant" <?php if($userinfo[7] === "Assistant"){echo "selected";} ?>>Assistant Professor</option>
+                                        <option value="Associate" <?php if($userinfo[7] === "Associate"){echo "selected";} ?>>Associate Professor</option>
+                                        <option value="Professor" <?php if($userinfo[7] === "Professor"){echo "selected";} ?>>Professor</option>
+                                        <option value="Emeritus" <?php if($userinfo[7] === "Emeritus"){echo "selected";} ?>>Professor Emeritus</option>
+                                    </optgroup>
+                                </select>
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr> <!--Blank for spacing-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Email-->
+                            <td>
+                                <label class = "edit_acc_label"> Email: </label>
+                            </td>
+                            <td>
+                                <input type="email" name="email" id="email" value="<?php if(!$error){echo "$userinfo[0]";} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Confirm Email-->
+                            <td>
+                                <label class = "edit_acc_label"> Confirm Email: </label>
+                            </td>
+                            <td>
+                                <input type="email" name="confirmemail" id="confirmemail" value="<?php if(!$error){echo $userinfo[0];} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Blank for spacing-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Password-->
+                            <td>
+                                <label class = "edit_acc_label"> Password: </label>
+                            </td>
+                            <td>
+                                <input type="password" name="password" id="password" value="<?php if(!$error){echo "$userinfo[2]";} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Confirm Password-->
+                            <td>
+                                <label class = "edit_acc_label"> Confirm Password: </label>
+                            </td>
+                            <td>
+                                <input type="password" name="confirmpassword" id="confirmpassword" value="<?php if(!$error){echo "$userinfo[2]";} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Blank for spacing-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Security question-->
+                            <td>
+                                <label class = "edit_acc_label"> Security Question: </label>
+                            </td>
+                            <td>
+                                <input type="text" name="squestion" value="<?php if(!$error){echo "$userinfo[8]";} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <!--Answer-->
+                            <td>
+                                <label class = "edit_acc_label"> Answer: </label>
+                            </td>
+                            <td>
+                                <input type="text" name="sanswer" value="<?php if(!$error){echo "$userinfo[9]";} ?>">
+                            </td>
+
+                            <!--Blank-->
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <br>
+            <div style="text-align: left;">
+                <input type="submit" value="Submit" onclick="submitAccount()">&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="button" value="Cancel">
+            </div>
+        </div>
+    </main>
+
+</div>
+</body>
+
+<script>
+    function swapselection(){
+        //get elements from page
+        var studentselect = document.getElementById("studentyear");
+        var facultyselect = document.getElementById("facultyrank");
+        var acctype = document.getElementById("acctype");
+        var positionlabel = document.getElementById("positionlabel");
+
+        //change parts of page depending on student of faculty
+        if(acctype.options[acctype.selectedIndex].text === "Faculty")
+        {
+            studentselect.style.display = "none";
+            facultyselect.style.display = "inline";
+            positionlabel.innerText = "Rank:";
+        }
+        else
+        {
+            studentselect.style.display = "inline";
+            facultyselect.style.display = "none";
+            positionlabel.innerText = "Year:";
+        }
+
+    }
+
+    function submitAccount() {
+        //get elements from page
+        var pass = document.getElementById("password");
+        var confirmpass = document.getElementById("confirmpassword");
+        var email = document.getElementById("email");
+        var confirmemail = document.getElementById("confirmemail");
+        var submiterror = document.getElementById("submiterror");
+        var createform = document.getElementById("createform");
+        var cansubmit = true;
+
+        //reset the element for errors to a default state
+        submiterror.innerText = "";
+        submiterror.style.display = "none";
+
+        //check if pass is empty
+        if(pass.value === "")
+        {
+            cansubmit = false;
+            submiterror.innerText = "Password is empty. \n";
+            submiterror.style.display = "block";
+        }
+        //check if pass and confirm pass are not the same
+        if (pass.value !== confirmpass.value)
+        {
+            cansubmit = false;
+            submiterror.innerText = submiterror.innerText.concat("Password and Confirm Password are not the same. \n");
+            submiterror.style.display = "block";
+        }
+        //check if email is empty
+        if(email.value === "")
+        {
+            cansubmit = false;
+            submiterror.innerText = submiterror.innerText.concat("Email is empty. \n");
+            submiterror.style.display = "block";
+        }
+        //check if email and confirmemail are not the same
+        if (email.value !== confirmemail.value)
+        {
+            cansubmit = false;
+            submiterror.innerText = submiterror.innerText.concat("Email and Confirm Email are not the same. \n");
+        }
+        if(cansubmit)
+        {
+            createform.submit();
+        }
+    }
+
+</script>
+
+</html>
---
+++ src/CreateAccountUpdateLogic.php
@@ -0,0 +1,50 @@
+<?php
+/*Ensure the database was initialized and obtain db link*/
+include_once '../config/ConfigV2.php';
+
+/*Get information from the search (post) request*/
+$acctype = $_POST['acctype'];
+$password = $_POST['password'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$dob = dateToUTC($_POST['dob']); //is converted to UTC
+$email = strtolower($_POST['email']); //is converted to lower
+$studentyear = $_POST['studentyear']; //only if student, ensure null otherwise
+$facultyrank = $_POST['facultyrank']; //only if faculty, ensure null otherwise
+$squestion = $_POST['squestion'];
+$sanswer = $_POST['sanswer'];
+$prevemail = $_POST['prevemail']; //required to find the user being updated
+
+/*Checking studentyear and facultyrank*/
+if($acctype === "Student")
+{
+    $facultyrank = null;
+}
+else if($acctype === "Faculty")
+{
+    $studentyear = null;
+}
+
+/*Update the database with the new info*/
+$query = "INSERT INTO User VALUES ('$email', '$acctype', '$password', '$fname', '$lname', '$dob', '$studentyear', '$facultyrank', '$squestion', '$sanswer')";
+$results = $db->query($query);
+
+//is true on success and false on failure
+if(!$results)
+{
+    echo "An error occurred.";
+}
+else
+{
+    header("Location: ../public/user_search.php");
+}
+
+
+function dateToUTC (String $date): ?string //expects dd/mm/yyyy and returns yyyy-mm-dd or null
+{
+    list($dd,$mm,$yyyy) = explode('/', $date);
+    if (!checkdate($mm,$dd,$yyyy)) {
+        return null;
+    }
+    return $utcdate = $yyyy . '-' . $mm . '-' . $dd;
+}
