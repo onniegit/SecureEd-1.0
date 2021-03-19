188fbe325fd97517931707e30d2bdd200d982331
Brandon Steed <brdsteed@gmail.com>
2021-03-19 06:28:23 -0400

    Progress on Enter Grades

---
+++ public/enter_grades.php
@@ -0,0 +1,73 @@
+<?php
+/*Check if DB was initialized and grab link*/
+$GLOBALS['dbPath'] = '../db/persistentconndb.sqlite';
+$db = new SQLite3($GLOBALS['dbPath'],  $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE , $encryptionKey = "");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="secure_app.css">
+    <meta charset="utf-8" />
+    <title>Secure App - Create Account</title>
+
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <table class="header_table">
+
+            <tbody>
+            <tr>
+
+                <td class="lock"><img src="Header_Lock_Image.svg" style="width:9vh;" alt=""></td>
+
+                <td class="title"><b>Secure App</b></td>
+
+                <td class="header_table_cell"></td>
+
+            </tr>
+            </tbody>
+        </table>
+    </header>
+
+    <main>
+        <!--Navigation Buttons-->
+        <nav>
+            <button class="button_large" type="button" onclick=" location.href = 'dashboard.php'">Dashboard</button>
+            <button class="button_large" type="button" onclick=" location.href = '../src/logout.php'">Log Out</button>
+        </nav>
+
+        <br>
+
+
+
+        <!--Heading-->
+        <h1>Create Account</h1>
+        <div class="horizontal_line">
+            <hr>
+        </div>
+
+        <p id="submiterror" style="display:none"></p>
+
+        <div style="text-align:center">
+            <div style="text-align:center;">
+                <form action="../src/EnterGradesUpdateLogic.php" method="POST" enctype="multipart/form-data">
+                    <div style="text-align:left">
+                        Course ID: <input type="text" id="crn">Enter Course ID here...</input>
+
+                        <input type="file" name="file" accept=".csv"/>
+
+                    </div>
+                </form>
+            </div>
+            <br>
+            <div style="text-align: left;">
+                <input type="submit" value="Import">&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="button" value="Cancel">
+            </div>
+        </div>
+    </main>
+</div>
+</body>
+</html>
---
+++ src/EnterGradesUpdateLogic.php
@@ -0,0 +1,36 @@
+<?php
+/*Ensure the database was initialized and obtain db link*/
+$GLOBALS['dbPath'] = '../db/persistentconndb.sqlite';
+$db = new SQLite3($GLOBALS['dbPath'],  $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE , $encryptionKey = "");
+
+
+
+if(isset($POST['submit']))
+{
+    if ($_FILES['file']['name'])
+    {
+        $filenames = explode(".", $_FILES['file']['name']);
+
+        if ($filename[1]=='csv')
+        {
+            $handle = fopen($_FILES['file']['tmp_name'],"r");
+
+            while ($data = fgetcsv($handle))
+            {
+                $crn = $data[0];
+                $student_id = $data[1];
+                $grade = $data[0];
+                $query = "SELECT StudentID FROM Enrollment WHERE CRN = '$crn'";
+                $results = $db->query($query);
+                $db->exec("INSERT INTO Grade (CRN, StudentID, Grade) values ('$crn', '$student_id', '$grade')");
+            }
+            fclose($handle);
+            echo "Grades have been updated.";
+        }
+    }
+}
+
+    //backup database
+    $db->backup($GLOBALS['dbPath'], $db, $db);
+    //redirect
+    header("Location: ../public/dashboard.php");
