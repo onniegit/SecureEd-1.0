a2de2c67a05a1e6c17e79cb7b2b17c63f81d72b4
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-03-18 20:40:28 -0400

    Update ForgotPassword.js

--- resources/ForgotPassword.js
+++ resources/ForgotPassword.js
@@ -1,27 +1,44 @@
 var pagetype = 1;
 var contents = document.getElementById("ForgotPasswordContent");
-var email = document.getElementById("emailform");
+var emailform = document.getElementById("emailform")
+var emailrequest = new XMLHttpRequest();
+
+
+function emailcheck(e)
+{
+ e.preventDefault();
+ pagetype++;
+}
+function newpassword(e)
+{
+ e.preventDefault();
+ location.replace("../public/index.php");
+}
+
 
  if(pagetype ==1)
  {
    contents.innerHTML = '<p1>Please enter your Email</p1> <br><br>'+'<form id="emailform">'+
      '<label style="float: center" for="email">Email:&nbsp;&nbsp;</label>'+
      '<input type="text" id="email" name="email"><br><br>'+
-     '<input type="submit" value="Submit">';
+       '<button type="submit" id= "submit1">Submit</button>';
+
+   emailform.addEventListener("submit",emailcheck)
 
  }
 
-email.addEventListener("submit",(e) =>{
- e.preventDefault();
 
-})
+
+
 
 if(pagetype ==2)
 {
  contents.innerHTML = '<form id="SecQform">'+
      '<label style="float: center" for="secR">Answer:&nbsp;&nbsp;</label>'+
      '<input type="text" id="secR" name="secR"><br><br>'+
-     '<input type="submit" value="Submit">';
+     '<button type="submit">Submit</button>';
+
+ emailform.addEventListener("Submit",emailcheck)
 
 }
 
@@ -33,5 +50,7 @@ if(pagetype ==3)
      '<label style="float: center" for="newpasswordconfirm">New password confirm:&nbsp;&nbsp;</label>'+
      '<input type="text" id="newpasswordconfirm" name="newpasswordconfirm"><br><br>'+
      '<input type="submit" value="Submit">';
+ emailform.addEventListener("submit",newpassword)
+
 
 }
