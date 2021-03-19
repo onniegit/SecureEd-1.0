d10145b098bc489b9d50ecaed82615e6f20ec2f5
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-03-18 23:33:19 -0400

    update to forgot password

--- resources/ForgotPassword.js
+++ resources/ForgotPassword.js
@@ -1,54 +1,52 @@
-var pagetype = 1;
 var contents = document.getElementById("ForgotPasswordContent");
-const emailform = document.getElementById("emailform")
+const Eform = document.getElementById("emailform")
 var emailrequest = new XMLHttpRequest();
+
+contents.innerHTML = '<p1>Please enter your Email</p1> <br><br>'+'<form id="emailform">'+
+    '<label style="float: center" for="email">Email:&nbsp;&nbsp;</label>'+
+    '<input type="text" id="email" name="email"><br><br>'+
+    '<input type="submit" value = "submit">';
+
+
+
+document.getElementById("emailform").addEventListener("submit",emailcheck)
+
+
 
 
 function emailcheck(e)
 {
  e.preventDefault();
- pagetype++;
-}
-function newpassword(e)
-{
- e.preventDefault();
- location.replace("../public/index.php");
-}
 
-
- if(pagetype ==1)
- {
-   contents.innerHTML = '<p1>Please enter your Email</p1> <br><br>'+'<form id="emailform">'+
-     '<label style="float: center" for="email">Email:&nbsp;&nbsp;</label>'+
-     '<input type="text" id="email" name="email"><br><br>'+
-       '<input type="submit" value = "Submit">';
-
- }
-
-emailform.addEventListener("submit",emailcheck)
-
-
-
-if(pagetype ==2)
-{
  contents.innerHTML = '<form id="SecQform">'+
      '<label style="float: center" for="secR">Answer:&nbsp;&nbsp;</label>'+
      '<input type="text" id="secR" name="secR"><br><br>'+
      '<button type="submit">Submit</button>';
-
- emailform.addEventListener("Submit",emailcheck)
-
+ document.getElementById("SecQform").addEventListener("submit",answercheck)
 }
 
-if(pagetype ==3)
+function answercheck(e)
 {
+ e.preventDefault();
+
  contents.innerHTML = '<p1>Please enter your new password</p1> <br><br>'+'<form id="passwordform">'+
      '<label style="float: center" for="newpassword">New password:&nbsp;&nbsp;</label>'+
      '<input type="text" id="newpassword" name="newpassword"><br><br>'+
      '<label style="float: center" for="newpasswordconfirm">New password confirm:&nbsp;&nbsp;</label>'+
      '<input type="text" id="newpasswordconfirm" name="newpasswordconfirm"><br><br>'+
      '<input type="submit" value="Submit">';
- emailform.addEventListener("submit",newpassword)
-
+  document.getElementById("passwordform").addEventListener("submit",newpassword)
 
 }
+
+
+function newpassword(e)
+{
+
+ e.preventDefault();
+ location.replace("../public/index.php");
+}
+
+
+
+
