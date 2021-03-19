fe564ff3dada9d098482fa89e1b0166d89d635f4
Lowell Ryan Holden <35051795+jda77@users.noreply.github.com>
2021-03-18 22:58:53 -0400

    Shows all contents of DB now

--- test/DBcontents.php
+++ test/DBcontents.php
@@ -1,22 +1,76 @@
 <?php
-include('../config/ConfigV2.php');
+$GLOBALS['dbPath'] = '../db/persistentconndb.sqlite';
+$db = new SQLite3($GLOBALS['dbPath'],  $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE , $encryptionKey = "");
+
 
 $sql =<<<EOF
 Select * From User;
 EOF;
-Global $db;
 $ret = $db->query($sql);
+echo "<h1>User Table Contents</h1>";
 while($row = $ret->fetchArray(SQLITE3_ASSOC) )
 {
-    echo "Email = " . $row['Email'] . "\n";
-    echo "AccountType = " . $row['AccountType'] . "\n";
-    echo "Password = " . $row['Password'] . "\n";
-    echo "Name = " . $row['Name'] . "\n";
-    echo "DOB = " . $row['DOB'] . "\n";
-    echo "Year = " . $row['Year'] . "\n";
-    echo "Rank = " . $row['Rank'] . "\n";
-    echo "Security Question = " . $row['SQuestion'] . "\n";
-    echo "Security Answer = " . $row['SAnswer'] . "\n";
+    echo "<p> UserID = " . $row['UserID'] . "\n</p>";
+    echo "<p>Email = " . $row['Email'] . "\n</p>";
+    echo "<p>AccType = " . $row['AccType'] . "\n</p>";
+    echo "<p>Password = " . $row['Password'] . "\n</p>";
+    echo "<p>Name = " . $row['FName'] . ',' . $row['LName'] . "\n</p>";
+    echo "<p>DOB = " . $row['DOB'] . "\n</p>";
+    echo "<p>Year = " . $row['Year'] . "\n</p>";
+    echo "<p>Rank = " . $row['Rank'] . "\n</p>";
+    echo "<p>Security Question = " . $row['SQuestion'] . "\n</p>";
+    echo "<p>Security Answer = " . $row['SAnswer'] . "\n</p>";
 }
 
+$sql =<<<EOF
+Select * From Course;
+EOF;
+$ret = $db->query($sql);
+echo "<h1>Course Table Contents</h1>";
+while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+{
+    echo "<p>Code = " . $row['Code'] . "\n</p>";
+    echo "<p>CourseName = " . $row['CourseName'] . "\n</p>";
+}
+
+$sql =<<<EOF
+Select * From Section;
+EOF;
+$ret = $db->query($sql);
+echo "<h1>Section Table Contents</h1>";
+while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+{
+    echo "<p>CRN = " . $row['CRN'] . "\n</p>";
+    echo "<p>Instructor = " . $row['Instructor'] . "\n</p>";
+    echo "<p>Course = " . $row['Course'] . "\n</p>";
+    echo "<p>Semester = " . $row['Semester'] . "\n</p>";
+    echo "<p>SectionLetter = " . $row['SectionLetter'] . "\n</p>";
+    echo "<p>StartTime = " . $row['StartTime'] . "\n</p>";
+    echo "<p>EndTime = " . $row['EndTime'] . "\n</p>";
+    echo "<p>Year = " . $row['Year'] . "\n</p>";
+    echo "<p>Location = " . $row['Location'] . "\n</p>";
+}
+
+$sql =<<<EOF
+Select * From Enrollment;
+EOF;
+$ret = $db->query($sql);
+echo "<h1>Enrollment Table Contents</h1>";
+while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+{
+    echo "<p>CRN = " . $row['CRN'] . "\n</p>";
+    echo "<p>StudentID = " . $row['StudentID'] . "\n</p>";
+}
+
+$sql =<<<EOF
+Select * From Grade;
+EOF;
+$ret = $db->query($sql);
+echo "<h1>Grade Table Contents</h1>";
+while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+{
+    echo "<p>CRN = " . $row['CRN'] . "\n</p>";
+    echo "<p>StudentID = " . $row['StudentID'] . "\n</p>";
+    echo "<p>Grade = " . $row['Grade'] . "\n</p>";
+}
 ?>
