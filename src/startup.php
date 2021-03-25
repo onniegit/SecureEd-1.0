33fde328fd86730c4685688c1bb79f4abb7fd367
Brandon Steed <brdsteed@gmail.com>
2021-03-24 20:40:47 -0400

    startup.php implemented
    Nukes the old database if it's still around on startup, then calls the the config file to rebuild the database from scratch.

--- src/startup.php
+++ src/startup.php
@@ -1,19 +1,9 @@
 <?php
 
-$callConfig = shell_exec('php ../config/ConfigV2.php');
-$dbPath = '../db/persistentconndb.sqlite';
+$GLOBALS['dbPath'] = 'db/persistentconndb.sqlite';
 
-//Check if persistentconndb.sqlite exists
-if(file_exists($dbPath)) {
-    ATTACH ../db/persistentconndb.sqlite AS db;
-    //If yes, run config if the database is empty
-
-    if (SELECT name FROM $db {
-        echo "<pre>$callConfig</pre>";
-    }
-} else { //If persistentconndb.sqlite doesn't exist, run config.
-    echo "<pre>$callConfig</pre>";
+if(file_exists($GLOBALS['dbPath'])) {
+    unlink($GLOBALS['dbPath']);
 }
 
-//Redirect to /public/index.php
-header("Location: ../public/login.php");
+shell_exec('php config/ConfigV2.php');
