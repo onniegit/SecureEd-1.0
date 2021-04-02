259ccac3dcc9624050bca2f5282ced0bcb3856e4
Brandon Steed <brdsteed@gmail.com>
2021-04-02 01:29:04 -0400

    Update EnterGradesUpdateLogic.php

--- src/EnterGradesUpdateLogic.php
+++ src/EnterGradesUpdateLogic.php
@@ -1,22 +1,23 @@
 <?php
 try {
-    /*Ensure the database was initialized and obtain db link*/
+    //Ensure the database was initialized and obtain db link
     $GLOBALS['dbPath'] = '../db/persistentconndb.sqlite';
-    $db = new SQLite3($GLOBALS['dbPath'], $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE, $encryptionKey = "");
+    $db = new SQLite3($GLOBALS['dbPath'],  $flags = SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE , $encryptionKey = "");
 
-    if (isset($_POST['submit'])) { //Checks if var is set
-        $crn = $_POST['crn']; //grab the CRN from form
-        $path = pathinfo($_FILES['file']['name']); //grabs info for the path
+    if (isset($_POST['submit'])) { //checks if submit var is set
+        $handle = fopen(($_FILES['file']['tmp_name']), "r"); //sets a read-only pointer at beginning of file
+        $crn = $_POST['crn']; //grabs CRN from form
+        $path = pathinfo($_FILES['file']['name']); //path info for file
 
-        if (path['extension'] == 'csv') { //is file a CSV?
-            $handle = fopen(($_FILES['file']['tmp_name']), "r"); //set a read-only pointer at beginning of file
-            while (($data = fgetcsv($handle, 100000, ",")) !== FALSE) { //iterate through file
-                $db->exec("INSERT INTO Grade VALUES ('$crn', '$data[0]', '$data[1]')");
-                //grab info from csv and write to DB
+        if($path['extension'] == 'csv') { //check if file is .csv
+            while (($data = fgetcsv($handle, 9001, ",")) !== FALSE) { //iterate through csv
+                $db->exec("INSERT INTO Grade VALUES ('$crn', '$data[0]', '$data[1]')"); //populate db from csv
             }
-            $db->backup($db, "temp", $GLOBALS['dbPath']); //backup DB
-            fclose($handle); //close pointer
+
+            $db->backup($db, "temp", $GLOBALS['dbPath']);
+            fclose($handle);
         }
+
         header("Location: ../test/DBcontents.php");
     }
 }
