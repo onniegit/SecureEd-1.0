8319dcdff4db13a2c9cb4b8f8d9ef9a85e5d9fd2
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-04-02 00:09:10 -0400

    error

--- src/ForgotPasswordSecQLogic.php
+++ src/ForgotPasswordSecQLogic.php
@@ -8,14 +8,17 @@ $db = new SQLite3($GLOBALS['dbPath'],  $flags = SQLITE3_OPEN_READWRITE | SQLITE3
 $email = "";
 $SecAnswer="";
 $mySAnswer = $_POST["Answer"];
-
+try{
 //opening tmp file for email
 $filename ="../resources/tmp.txt";
 $file =fopen($filename,"r+");
 $email = fread($file,filesize($filename));
 
+
+
 $query = "SELECT SAnswer FROM User WHERE Email ='$email'";
 $SecAnswer = $db->querySingle($query);
+
 //intentionally made this wrong to skip check, couldn't get it to have a right answer
 if($mySAnswer == $SecAnswer) {
     header("Location:../public/ForgotPasswordChange.php");
@@ -24,9 +27,11 @@ if($mySAnswer == $SecAnswer) {
 else{
     header("Location:../public/ForgotPasswordSecQ.php?answercheck=fail");
 }
-
-
-
+}
+catch(Exception $e)
+{
+    header("Location:../resources/Errorpage.php");
+}
 
 
 
