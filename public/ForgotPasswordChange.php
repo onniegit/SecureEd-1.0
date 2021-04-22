4f983b07c9fb001a2b9b2a0d0bd5b646e390f977
Davlee1 <54450596+Davlee1@users.noreply.github.com>
2021-04-22 18:51:45 -0400

    minor alignment adjustment

--- public/ForgotPasswordChange.php
+++ public/ForgotPasswordChange.php
@@ -29,7 +29,7 @@
                 <hr>
             </div>
 
-            <div class = "NewPassword" style="text-align:center">
+            <div class = "NewPassword" style="text-align:right" >
                 <div style="text-align:center">
                 <?php
                          $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
