e5fa192038af75ec4fdb43aa33c8701638eef5df
Lowell Ryan Holden <35051795+jda77@users.noreply.github.com>
2021-05-06 00:50:29 -0400

    Minor code clean up and comments
    Added a bit of documentation and removed some unnecessary variables.

--- src/EnterGradesUpdateLogic.php
+++ src/EnterGradesUpdateLogic.php
@@ -4,23 +4,25 @@ try {
     require_once "../src/DBController.php";
 
     if (isset($_POST['submit'])) { //checks if submit var is set
-        $currentDirectory = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') ;
+        $currentDirectory = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..');//get root directory
         $uploadDirectory = "\uploads\\";
 
+        //get info about the file
         $filename = $_FILES['file']['name'];
-        $filesize = $_FILES['file']['size'];
         $filetmp  = $_FILES['file']['tmp_name'];
-        $fileType = $_FILES['file']['type'];
 
+        //create the upload path with the original filename
         $uploadPath = $currentDirectory . $uploadDirectory . basename($filename);
 
+        //copy file to uploads folder
         copy($filetmp, $uploadPath);
 
+        //prepare vars to insert data into database
         $handle = fopen(($_FILES['file']['tmp_name']), "r"); //sets a read-only pointer at beginning of file
         $crn = $_POST['crn']; //grabs CRN from form
         $path = pathinfo($_FILES['file']['name']); //path info for file
 
-
+        //insert data into the database if csv
         if($path['extension'] == 'csv') { //check if file is .csv
             while (($data = fgetcsv($handle, 9001, ",")) !== FALSE) { //iterate through csv
                 $crn = $db->escapeString($crn); //sanitize the crn
